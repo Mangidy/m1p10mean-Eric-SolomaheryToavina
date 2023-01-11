b4f8650cfbf18710b7cfae2211ecffc48b6cfd9e
Mangidy <maherymangidy@gmail.com>
2023-01-11 19:10:27 +0300

    deposit and home template

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepositComponent } from './deposit/deposit.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path:'login', component: LoginComponent}
+  { path:'', component: HomeComponent},
+  { path:'login', component: LoginComponent},
+  { path:'deposit', component: DepositComponent}
 ];
 
 @NgModule({
