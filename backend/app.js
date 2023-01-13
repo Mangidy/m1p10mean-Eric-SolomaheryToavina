274fb421e4bc8c95d248c3fdc425abcf64cfb09a
Ratovonirina <eric.ratovonirina@gmail.com>
2023-01-13 20:15:45 +0300

    Connect to DataBase

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,22 @@
 const express = require('express')
+const { MongoClient } = require('mongodb');
 
 const app = express()
 const uriBd = "mongodb://localhost:27017/"
 
 const start = (port, route) => {
-    app.use(express.urlencoded({ extended: true }))
-    app.use(express.json())
+    MongoClient.connect(uriBd, { useUnifiedTopology: true })
+        .then(client => {
+            console.log("DB Connected");
 
-    app.get("/", route.home)
+            app.use(express.urlencoded({ extended: true }))
+            app.use(express.json())
 
-    app.listen(port, console.log(`Server running on port ${port}`))
+            app.get("/", route.home)
+
+            app.listen(port, console.log(`Server running on port ${port}`))
+        })
+        .catch(error => console.error(error))
 }
 
 exports.start = start
