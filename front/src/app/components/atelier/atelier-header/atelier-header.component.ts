51205fb17fa09ce2fd7caff82ffa78c53235c608
Ratovonirina <eric.ratovonirina@gmail.com>
2023-01-25 10:15:56 +0300

    Rectification

--- front/src/app/components/atelier/atelier-header/atelier-header.component.ts
+++ front/src/app/components/atelier/atelier-header/atelier-header.component.ts
@@ -5,31 +5,32 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-atelier-header',
   templateUrl: './atelier-header.component.html',
-  styleUrls: ['./atelier-header.component.css']
+  styleUrls: ['./atelier-header.component.css'],
 })
 export class AtelierHeaderComponent {
-  constructor(private auth:AuthService,private router:Router){}
-  
- 
-  logout(): void{
-   this.auth.logout();
-   this.auth.logoutAdmin().subscribe((val => console.log(val)));
+  constructor(private auth: AuthService, private router: Router) {}
+
+  logout(): void {
+    this.auth.logout();
+    this.auth.logoutAdmin().subscribe((val) => console.log(val));
   }
 
-
-takeHome():void{
-  if(Number(localStorage.getItem('privilage'))==1){
-    this.router.navigate(['client']);
+  takeHome(): void {
+    if (Number(localStorage.getItem('privilage')) == 1) {
+      this.router.navigate(['client']);
+    } else if (Number(localStorage.getItem('privilage')) == 2) {
+      this.router.navigate(['atelier']);
+      console.log(
+        console.log(
+          this.auth
+            .getOneCar('63d0c45b9076d8e5fb3b8b0d')
+            .subscribe((val) => console.log(val))
+        )
+      );
+    } else if (Number(localStorage.getItem('privilage')) == 3) {
+      this.router.navigate(['financier']);
+    } else if (Number(localStorage.getItem('privilage')) == 4) {
+      this.router.navigate(['admin']);
+    }
   }
-  else if(Number(localStorage.getItem('privilage'))==2){
-    this.router.navigate(['atelier']);
-    console.log(console.log(this.auth.getAllCarReception().subscribe((val => console.log(val)))));
-  }
-  else if(Number(localStorage.getItem('privilage'))==3){
-    this.router.navigate(['financier']);
-  }
-  else if(Number(localStorage.getItem('privilage'))==4){
-    this.router.navigate(['admin']);
-  }
-}
 }
--- front/src/app/services/auth.service.ts
+++ front/src/app/services/auth.service.ts
@@ -4,82 +4,108 @@ import { Observable, of, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private router: Router,private http:HttpClient) {}
-  logAdmin({ username, password }: any): Observable<any>{
-    return this.http.post<any>("/api/admin/login",{ username, password });
+  constructor(private router: Router, private http: HttpClient) {}
+  logAdmin({ username, password }: any): Observable<any> {
+    return this.http.post<any>('/api/admin/login', { username, password });
   }
-  logClient({ email, password }: any): Observable<any>{
-    return this.http.post<any>("/api/client/login",{ email, password });
+  logClient({ email, password }: any): Observable<any> {
+    return this.http.post<any>('/api/client/login', { email, password });
   }
-  getAdmin(): Observable<any>{
-    return this.http.get<any>("/api/admin/");
+  getAdmin(): Observable<any> {
+    return this.http.get<any>('/api/admin/');
   }
-  getAllCar(): Observable<any>{
-    return this.http.get<any>("/api/admin/car");
+  getAllCar(): Observable<any> {
+    return this.http.get<any>('/api/admin/car');
   }
-  getAllCarReception(): Observable<any>{
-    return this.http.get<any>("/api/admin/car/reception");
+  getAllCarReception(): Observable<any> {
+    return this.http.get<any>('/api/admin/car/reception');
   }
-  getCarOut(): Observable<any>{
-    return this.http.get<any>("/api/admin/car/sortie");
+  getCarOut(): Observable<any> {
+    return this.http.get<any>('/api/admin/car/sortie');
   }
-  getAllFacture(): Observable<any>{
-    return this.http.get<any>("/api/admin/facture");
+  getAllFacture(): Observable<any> {
+    return this.http.get<any>('/api/admin/facture');
   }
-   //--------------not working yet---------------
-   getFactureTF(valeur: any): Observable<any>{
-    return this.http.get<any>("/api/admin/facture/:valeur",valeur);
+  //--------------not working yet---------------
+  getFactureTF(valeur: any): Observable<any> {
+    return this.http.get<any>('/api/admin/facture/:valeur', valeur);
   }
   //--------------------------------------------
 
   //--------------not working yet---------------
-  getOneCar(id: any): Observable<any>{
-    return this.http.get<any>("/api/admin/car/:id",id);
+  getOneCar(id: any): Observable<any> {
+    return this.http.get<any>('/api/admin/car/' + id);
   }
   //--------------------------------------------
 
-  getAllClient(): Observable<any>{
-    return this.http.get<any>("/api/admin/client");
-  } 
+  getAllClient(): Observable<any> {
+    return this.http.get<any>('/api/admin/client');
+  }
 
   //--------------not working yet---------------
-  getOneClient(_id: any): Observable<any>{
-    return this.http.get<any>("/api/admin/client/:id" ,_id);
+  getOneClient(_id: any): Observable<any> {
+    return this.http.get<any>('/api/admin/client/:id', _id);
   }
   //--------------------------------------------
 
   //--------------not tested yet---------------
-  addCarReparation({ numero}: any): Observable<any>{
-    return this.http.post<any>("/api/admin/car/reparation/:numero",{ numero});
+  addCarReparation({ numero }: any): Observable<any> {
+    return this.http.post<any>('/api/admin/car/reparation/:numero', { numero });
   }
   //--------------------------------------------
 
   //--------------not tested yet---------------
-  addCarFacture({ idVoiture}: any): Observable<any>{
-    return this.http.post<any>("/api/admin/car/facture/:idVoiture",{ idVoiture});
+  addCarFacture({ idVoiture }: any): Observable<any> {
+    return this.http.post<any>('/api/admin/car/facture/:idVoiture', {
+      idVoiture,
+    });
   }
   //--------------------------------------------
-  addAdmin({ usernameAdmin, passwordAdmin,roleAdmin }: any): Observable<any>{
-    return this.http.post<any>("/api/admin/add",{ usernameAdmin, passwordAdmin,roleAdmin});
+  addAdmin({ usernameAdmin, passwordAdmin, roleAdmin }: any): Observable<any> {
+    return this.http.post<any>('/api/admin/add', {
+      usernameAdmin,
+      passwordAdmin,
+      roleAdmin,
+    });
   }
-  logoutAdmin(): Observable<any>{
-    return this.http.post<any>("/api/admin/logout",null);
+  logoutAdmin(): Observable<any> {
+    return this.http.post<any>('/api/admin/logout', null);
   }
-  addClient({ username, password,nom,prenom,adress,phone,email }: any): Observable<any>{
-    return this.http.post<any>("/api/client/subscribe",{ username, password,nom,prenom,adress,phone,email });
+  addClient({
+    username,
+    password,
+    nom,
+    prenom,
+    adress,
+    phone,
+    email,
+  }: any): Observable<any> {
+    return this.http.post<any>('/api/client/subscribe', {
+      username,
+      password,
+      nom,
+      prenom,
+      adress,
+      phone,
+      email,
+    });
   }
-  addCarClient({ numero, marque, modele, annee}: any): Observable<any>{
-    return this.http.post<any>("/api/client/car",{ numero, marque, modele, annee });
+  addCarClient({ numero, marque, modele, annee }: any): Observable<any> {
+    return this.http.post<any>('/api/client/car', {
+      numero,
+      marque,
+      modele,
+      annee,
+    });
   }
- 
-  logoutClient(): Observable<any>{
-    return this.http.post<any>("/api/client/logout",null);
+
+  logoutClient(): Observable<any> {
+    return this.http.post<any>('/api/client/logout', null);
   }
-  setPrivilage(a: string):void{
+  setPrivilage(a: string): void {
     localStorage.setItem('privilage', a);
   }
   getPrivilage() {
@@ -97,8 +123,6 @@ export class AuthService {
     return this.getToken() !== null;
   }
 
-
-
   logout() {
     localStorage.removeItem('token');
     this.router.navigate(['home']);
@@ -113,17 +137,17 @@ export class AuthService {
     if (email === 'admin@gmail.com' && password === 'admin123') {
       this.setToken('abcdefghijklmnopqrstuvwxyz');
       this.setPrivilage('2');
-      return of({ name: 'Solomahery', email: 'admin@gmail.com'});
+      return of({ name: 'Solomahery', email: 'admin@gmail.com' });
     }
     if (email === 'admin@gmail.com' && password === 'admin321') {
       this.setToken('abcdefghijklmnopqrstuvwxyz');
       this.setPrivilage('3');
-      return of({ name: 'Solomahery', email: 'admin@gmail.com'});
+      return of({ name: 'Solomahery', email: 'admin@gmail.com' });
     }
     if (email === 'admin@gmail.com' && password === 'admin') {
       this.setToken('abcdefghijklmnopqrstuvwxyz');
       this.setPrivilage('4');
-      return of({ name: 'Solomahery', email: 'admin@gmail.com'});
+      return of({ name: 'Solomahery', email: 'admin@gmail.com' });
     }
     return throwError(new Error('Failed to login'));
   }
