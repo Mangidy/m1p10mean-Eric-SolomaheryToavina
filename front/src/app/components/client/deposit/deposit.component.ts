bb0543e60042784e5126493e6f0872bd87a927ab
Ratovonirina <eric.ratovonirina@gmail.com>
2023-01-27 19:19:46 +0300

    client activite design

--- front/src/app/components/client/deposit/deposit.component.ts
+++ front/src/app/components/client/deposit/deposit.component.ts
@@ -5,50 +5,50 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-deposit',
   templateUrl: './deposit.component.html',
-  styleUrls: ['./deposit.component.css']
+  styleUrls: ['./deposit.component.css'],
 })
 export class DepositComponent {
   depositForm = new FormGroup({
-    numero: new FormControl('',[
-      Validators.required]),
-    marque: new FormControl('',[
-      Validators.required]),
-    modele: new FormControl('',[
-      Validators.required]),
-    annee: new FormControl('',[
-      Validators.required]),
+    numero: new FormControl('', [Validators.required]),
+    marque: new FormControl('', [Validators.required]),
+    modele: new FormControl('', [Validators.required]),
+    annee: new FormControl('', [Validators.required]),
   });
-  get numero(){
+  get numero() {
     return this.depositForm.get('numero');
-    }
-  get marque(){
-    return this.depositForm.get('marque');
-   }
-  get modele(){
-     return this.depositForm.get('modele');
-    }
-  get annee(){
-     return this.depositForm.get('annee');
-    }
-  constructor(private auth:AuthService){
-
   }
-  onSubmit():void{
+  get marque() {
+    return this.depositForm.get('marque');
+  }
+  get modele() {
+    return this.depositForm.get('modele');
+  }
+  get annee() {
+    return this.depositForm.get('annee');
+  }
+  constructor(private auth: AuthService) {}
+  onSubmit(): void {
     if (this.depositForm.valid) {
       console.log(this.depositForm.value.numero);
-    console.log(this.auth.addCarClient({ numero:this.depositForm.value.numero, marque:this.depositForm.value.marque, modele:this.depositForm.value.modele, annee:this.depositForm.value.annee}).subscribe((val =>{if (val.message='NEW CAR ADDED'){
-      alert("Deposit done!");
-    }
-    else{
-      alert(val.detailled);
-    }
-  }
-    )));
- 
-    }
-    else
-    {
-      alert("Missing details");
+      console.log(
+        this.auth
+          .addCarClient({
+            numero: this.depositForm.value.numero,
+            marque: this.depositForm.value.marque,
+            modele: this.depositForm.value.modele,
+            annee: this.depositForm.value.annee,
+          })
+          .subscribe((val) => {
+            if ((val.message = 'NEW CAR ADDED')) {
+              this.depositForm.reset();
+              alert('Nouvelle voiture ajoutée !');
+            } else {
+              alert(val.detailled);
+            }
+          })
+      );
+    } else {
+      alert('Missing details');
     }
   }
 }
