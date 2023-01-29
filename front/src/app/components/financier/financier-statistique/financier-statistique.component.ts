29fdeb884fd8048a3097b3f984a624b84ab3b5cc
Ratovonirina <eric.ratovonirina@gmail.com>
2023-01-29 14:12:49 +0300

    Financier Stat

--- front/src/app/components/financier/financier-statistique/financier-statistique.component.ts
+++ front/src/app/components/financier/financier-statistique/financier-statistique.component.ts
@@ -6,47 +6,59 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-financier-statistique',
   templateUrl: './financier-statistique.component.html',
-  styleUrls: ['./financier-statistique.component.css']
+  styleUrls: ['./financier-statistique.component.css'],
 })
 export class FinancierStatistiqueComponent {
-  statJour:any;
-  statMonth:any;
-  statReparation:any;
-  statBenef:any;
-  moyenne:any;
-  get value(){
+  loader: boolean;
+  statJour: any;
+  statMonth: any;
+  statReparation: any;
+  statBenef: any;
+  moyenne: any;
+  get value() {
     return this.statForm.get('value');
-    }
+  }
   statForm = new FormGroup({
-    param:new FormControl(''),
-    value: new FormControl('',[
-      Validators.required])
+    param: new FormControl(''),
+    value: new FormControl('', [Validators.required]),
   });
-  constructor(private auth:AuthService){}
+  constructor(private auth: AuthService) {
+    this.loader = true;
+  }
   ngOnInit() {
-
-
-  this.auth.getStatJour().subscribe((val) =>{ this.statJour=val;this.auth.getStatMonth().subscribe((val) =>{ this.statMonth=val;this.auth.getStatReparation().subscribe((val) =>{ var total=0; for(var i=0;i<val.length;i++){
-    total+=val[i].TempsReparation.jour*24;
- 
- this.statReparation=total/val.length;
+    this.auth.getStatJour().subscribe((val) => {
+      this.statJour = val;
+      this.auth.getStatMonth().subscribe((val) => {
+        this.statMonth = val;
+        this.auth.getStatReparation().subscribe((val) => {
+          var total = 0;
+          for (var i = 0; i < val.length; i++) {
+            total += val[i].TempsReparation.jour * 24;
+            this.statReparation = total / val.length;
+          }
+          this.loader = false;
+          console.log(val);
+        });
+        console.log(val);
+      });
+      console.log(val);
+    });
   }
-  
-   console.log( val);}); console.log( val);}); console.log( val);});
-
-
- 
-  }
-  
-
 
   objectKeys = Object.keys;
-  public voirBenef(){
-    if(this.statForm.value.param==''||this.statForm.value.value==''){
-      Swal.fire('Erreur','Veuillez remplir les valeurs','error');
-    }
-    else{
-      this.auth.getStatBenefice(this.statForm.value.param,this.statForm.value.value).subscribe((val) => {this.statBenef=val; console.log( val);});
+  public voirBenef() {
+    this.loader = true;
+    if (this.statForm.value.param == '' || this.statForm.value.value == '') {
+      this.loader = false;
+      Swal.fire('Erreur', 'Veuillez remplir les valeurs', 'error');
+    } else {
+      this.auth
+        .getStatBenefice(this.statForm.value.param, this.statForm.value.value)
+        .subscribe((val) => {
+          this.loader = false;
+          this.statBenef = val;
+          console.log(val);
+        });
     }
   }
 }
