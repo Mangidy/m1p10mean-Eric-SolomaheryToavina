7547d88a47cd92e6878530d88c0f140e55cb8c23
Ratovonirina <eric.ratovonirina@gmail.com>
2023-01-29 11:43:48 +0300

    Atelier Notificaton with Loader

--- front/src/app/components/atelier/atelier-notification/atelier-notification.component.ts
+++ front/src/app/components/atelier/atelier-notification/atelier-notification.component.ts
@@ -4,17 +4,20 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-atelier-notification',
   templateUrl: './atelier-notification.component.html',
-  styleUrls: ['./atelier-notification.component.css']
+  styleUrls: ['./atelier-notification.component.css'],
 })
 export class AtelierNotificationComponent {
-  data:any;
-  constructor(private auth:AuthService){}
+  data: any;
+  loader: boolean;
+  constructor(private auth: AuthService) {
+    this.loader = true;
+  }
   ngOnInit() {
-    
-  this.auth.adminNotificationClient().subscribe((val) =>{ this.data=val; console.log( val);});
-
+    this.auth.adminNotificationClient().subscribe((val) => {
+      this.data = val;
+      this.loader = false;
+      console.log(val);
+    });
   }
   objectKeys = Object.keys;
- 
-   
 }
