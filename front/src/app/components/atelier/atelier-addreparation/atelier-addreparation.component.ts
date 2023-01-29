4368c47a2cf85225c2e0d3521b94a3bec1dccef0
Ratovonirina <eric.ratovonirina@gmail.com>
2023-01-29 11:15:10 +0300

    Add reparation Atelier

--- front/src/app/components/atelier/atelier-addreparation/atelier-addreparation.component.ts
+++ front/src/app/components/atelier/atelier-addreparation/atelier-addreparation.component.ts
@@ -6,60 +6,65 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-atelier-addreparation',
   templateUrl: './atelier-addreparation.component.html',
-  styleUrls: ['./atelier-addreparation.component.css']
+  styleUrls: ['./atelier-addreparation.component.css'],
 })
 export class AtelierAddreparationComponent {
+  loader: boolean;
   repairForm = new FormGroup({
-    numero:new FormControl(''),
+    numero: new FormControl(''),
     params: new FormControl('', [Validators.required]),
-    value: new FormControl('', [Validators.required])
+    value: new FormControl('', [Validators.required]),
   });
-  get  params() {
+  get params() {
     return this.repairForm.get('params');
   }
   get value() {
     return this.repairForm.get('value');
   }
-  data:any;
- 
-  constructor(private auth:AuthService){}
-  ngOnInit() {
-    
-  this.auth.getAllCarReception().subscribe((val) =>{ this.data=val; console.log(val);});
+  data: any;
 
+  constructor(private auth: AuthService) {
+    this.loader = true;
+  }
+  ngOnInit() {
+    this.auth.getAllCarReception().subscribe((val) => {
+      this.data = val;
+      this.loader = false;
+      console.log(val);
+    });
   }
   onSubmit(): void {
     if (this.repairForm.valid) {
+      this.loader = true;
       console.log(this.repairForm.value.params);
-      console.log(this.repairForm.value.value); 
-     
-var newKey = this.repairForm?.value?.params?.toString();
-var info = {
+      console.log(this.repairForm.value.value);
 
+      var newKey = this.repairForm?.value?.params?.toString();
+      var info = {
+        [newKey as string]: this.repairForm.value.value?.toString(),
+      };
 
-  [newKey as string]: this.repairForm.value.value?.toString(),
-
-};
-
-      
-      
-       console.log(
-        this.auth.addCarReparation(this.repairForm.value.numero,this.repairForm.value.params,this.repairForm.value.value).subscribe((val) => {
-          console.log(val);
-          console.log(info);
+      console.log(
+        this.auth
+          .addCarReparation(
+            this.repairForm.value.numero,
+            this.repairForm.value.params,
+            this.repairForm.value.value
+          )
+          .subscribe((val) => {
+            console.log(val);
+            this.loader = false;
+            console.log(info);
             if ((val.message = 'CAR REPARATION ADDED')) {
               this.repairForm.reset();
-              
-     
-                Swal.fire('Sucess','Ajout effectuer!','success');
-              } else {
-                Swal.fire('erreur',val.detailled,'error');
-              }
+              Swal.fire('Sucess', 'Ajout effectuer!', 'success');
+            } else {
+              Swal.fire('erreur', val.detailled, 'error');
+            }
           })
       );
     } else {
-      Swal.fire('erreur','Detaille manquante','error');
+      Swal.fire('erreur', 'Detaille manquante', 'error');
     }
   }
 }
-
