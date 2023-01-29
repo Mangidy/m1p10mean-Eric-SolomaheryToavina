cc56c063b09b1ac1936e6edae5ad1095ce903927
Ratovonirina <eric.ratovonirina@gmail.com>
2023-01-29 10:20:07 +0300

    Login & Subscribe with loader

--- front/src/app/components/login/login.component.ts
+++ front/src/app/components/login/login.component.ts
@@ -1,65 +1,67 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  get email(){
+  loader: boolean;
+  get email() {
     return this.loginForm.get('email');
-    }
-    get password(){
-      return this.loginForm.get('password');
-      }
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
   loginForm = new FormGroup({
-    email: new FormControl('',[
+    email: new FormControl('', [
       Validators.required,
-      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
-    password: new FormControl('',[
-      Validators.required]),
+      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+    ]),
+    password: new FormControl('', [Validators.required]),
   });
 
-  constructor(private auth:AuthService, private router: Router){
-
+  constructor(private auth: AuthService, private router: Router) {
+    this.loader = false;
   }
 
-  onSubmit():void{
+  onSubmit(): void {
     if (this.loginForm.valid) {
+      this.loader = true;
       console.log(this.loginForm.value);
-       //this.auth.addAdmin(({ usernameAdmin:'ad', passwordAdmin:'admin',roleAdmin:'ADMIN'})).subscribe((val => console.log(val)));
+      //this.auth.addAdmin(({ usernameAdmin:'ad', passwordAdmin:'admin',roleAdmin:'ADMIN'})).subscribe((val => console.log(val)));
       //console.log(this.auth.getAllCar().subscribe((val => console.log(val))));
       this.auth.removeAllToken();
-      this.auth.logClient({email:this.loginForm.value.email,password:this.loginForm.value.password}).subscribe(val => { if(val.message=='LOGIN SUCCESSFULLY'){
-        this.router.navigate(['/client/home']);
-        }
-        else{
-          Swal.fire('Erreur','Email ou mot de passe incorrect','error');
-        }
-      }); 
-    /* this.auth.logClient({email:this.loginForm.value.email,password:this.loginForm.value.password}).subscribe(val => { if(val.message=='LOGIN SUCCESSFULLY'){
+      this.auth
+        .logClient({
+          email: this.loginForm.value.email,
+          password: this.loginForm.value.password,
+        })
+        .subscribe((val) => {
+          this.loader = false;
+          if (val.message == 'LOGIN SUCCESSFULLY') {
+            this.router.navigate(['/client/home']);
+          } else {
+            Swal.fire('Erreur', 'Email ou mot de passe incorrect', 'error');
+          }
+        });
+      /* this.auth.logClient({email:this.loginForm.value.email,password:this.loginForm.value.password}).subscribe(val => { if(val.message=='LOGIN SUCCESSFULLY'){
       this.router.navigate(['/client']);
       }
       else{
         alert('Incorrect email or password ');
       }
     }); */
-        
-    
-     
-//this.auth.logAdmin({username:'admin',password:'admin'}).subscribe((val => console.log(val)));
-      
-   
-     //console.log(this.auth.getAllClient().subscribe((val => console.log(val))));
-  //console.log(this.auth.getAdmin().subscribe((val => console.log(val))));
-     //this.auth.addAdmin(({ usernameAdmin:'admin99', passwordAdmin:'admin',roleAdmin:'1'})).subscribe((val => console.log(val)));
-    }
-    
-  }
-  
-}
 
+      //this.auth.logAdmin({username:'admin',password:'admin'}).subscribe((val => console.log(val)));
+
+      //console.log(this.auth.getAllClient().subscribe((val => console.log(val))));
+      //console.log(this.auth.getAdmin().subscribe((val => console.log(val))));
+      //this.auth.addAdmin(({ usernameAdmin:'admin99', passwordAdmin:'admin',roleAdmin:'1'})).subscribe((val => console.log(val)));
+    }
+  }
+}
--- front/src/app/components/sign-up/sign-up.component.ts
+++ front/src/app/components/sign-up/sign-up.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators, AbstractControl, ValidationErrors, ValidatorFn, FormBuilder} from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+  ValidatorFn,
+  FormBuilder,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import Swal from 'sweetalert2';
@@ -7,93 +15,111 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent {
-  get firstName(){
+  loader: boolean;
+  get firstName() {
     return this.signupForm.get('firstName');
-    }
-  get lastName(){
+  }
+  get lastName() {
     return this.signupForm.get('lastName');
-      }
-  get username(){
-   return this.signupForm.get('username');
-     }
-  get email(){
-     return this.signupForm.get('email');
-      }
-  get adress(){
+  }
+  get username() {
+    return this.signupForm.get('username');
+  }
+  get email() {
+    return this.signupForm.get('email');
+  }
+  get adress() {
     return this.signupForm.get('adress');
-    }
-  get phone(){
-     return this.signupForm.get('phone');
-    }
-  get password1(){
+  }
+  get phone() {
+    return this.signupForm.get('phone');
+  }
+  get password1() {
     return this.signupForm.get('password1');
-     }
-  get password2(){
-      return this.signupForm.get('password2');
-     }
-  
+  }
+  get password2() {
+    return this.signupForm.get('password2');
+  }
 
-    ConfirmPasswordValidator(controlName: string, matchingControlName: string) {
-      return (formGroup: FormGroup) => {
-        let control = formGroup.controls[controlName];
-        let matchingControl = formGroup.controls[matchingControlName]
-        if (
-          matchingControl.errors &&
-          !matchingControl.errors['confirmPasswordValidator']
-        ) {
-          return;
-        }
-        if (control.value !== matchingControl.value) {
-          matchingControl.setErrors({ confirmPasswordValidator: true });
-        } else {
-          matchingControl.setErrors(null);
-        }
-      };
+  ConfirmPasswordValidator(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      let control = formGroup.controls[controlName];
+      let matchingControl = formGroup.controls[matchingControlName];
+      if (
+        matchingControl.errors &&
+        !matchingControl.errors['confirmPasswordValidator']
+      ) {
+        return;
+      }
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ confirmPasswordValidator: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
+  }
+
+  signupForm = this.fb.group(
+    {
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      ]),
+      adress: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.required]),
+      password1: new FormControl('', [Validators.required]),
+      password2: new FormControl('', [Validators.required]),
+    },
+    {
+      validator: this.ConfirmPasswordValidator('password1', 'password2'),
     }
-    
-  signupForm = this.fb.group({
-    firstName: new FormControl('',[
-      Validators.required]),
-    lastName: new FormControl('',[
-      Validators.required]),
-    username: new FormControl('',[
-      Validators.required]),
-    email: new FormControl('',[
-      Validators.required,
-      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
-    adress: new FormControl('',[
-      Validators.required]),
-    phone: new FormControl('',[
-      Validators.required]),
-    password1: new FormControl('',[
-      Validators.required]), 
-    password2: new FormControl('',[
-      Validators.required]) },
-      {
-        validator: this.ConfirmPasswordValidator("password1", "password2")
-      });
-  
+  );
 
-  constructor(private auth:AuthService, private router: Router,private fb: FormBuilder){}
-    onSubmit():void {
-      if (this.signupForm.valid) {
-
-        console.log(this.signupForm.value);
-       //this.auth.logAdmin({username:'admin',password:'admin'}).subscribe((val => console.log(val)));
-       //console.log(this.auth.getAllClient().subscribe((val => console.log(val))));
-    //console.log(this.auth.getAdmin().subscribe((val => console.log(val))));
-    if(this.signupForm.value.password1==this.signupForm.value.password2){
-       this.auth.addClient(({ username:this.signupForm.value.username, password:this.signupForm.value.password2,nom:this.signupForm.value.firstName,prenom:this.signupForm.value.lastName,adress:this.signupForm.value.adress,phone:this.signupForm.value.phone,email:this.signupForm.value.email })).subscribe((val =>{if(val.message=="SUBSCRIBE SUCCESSFULLY") {
-                Swal.fire('Sucess','Utilisateur bien ajouter','success');
-              } else {
-                Swal.fire('erreur',val.detailled,'error');
-              }}));
-        this.router.navigate(['/login']);
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.loader = false;
+  }
+  onSubmit(): void {
+    if (this.signupForm.valid) {
+      this.loader = true;
+      console.log(this.signupForm.value);
+      //this.auth.logAdmin({username:'admin',password:'admin'}).subscribe((val => console.log(val)));
+      //console.log(this.auth.getAllClient().subscribe((val => console.log(val))));
+      //console.log(this.auth.getAdmin().subscribe((val => console.log(val))));
+      if (this.signupForm.value.password1 == this.signupForm.value.password2) {
+        this.auth
+          .addClient({
+            username: this.signupForm.value.username,
+            password: this.signupForm.value.password2,
+            nom: this.signupForm.value.firstName,
+            prenom: this.signupForm.value.lastName,
+            adress: this.signupForm.value.adress,
+            phone: this.signupForm.value.phone,
+            email: this.signupForm.value.email,
+          })
+          .subscribe((val) => {
+            this.loader = false;
+            if (val.message == 'SUBSCRIBE SUCCESSFULLY') {
+              Swal.fire(
+                'Sucess',
+                'Vous ête inscrit(e), connectez-vous maintenant',
+                'success'
+              );
+              this.router.navigate(['/login']);
+            } else {
+              Swal.fire('erreur', val.detailled, 'error');
+            }
+          });
       }
     }
+  }
 }
-}
-
